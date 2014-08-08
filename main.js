975071d6f7abab6bd6e446230d0a2bcbf530afa2
Ryan Zabcik <ryanzabcik@gmail.com>
2014-08-07 19:43:41 -0500

    remove debugger statement

--- main.js
+++ main.js
@@ -2,6 +2,5 @@ window.onload = function () {
     var roll_elem = document.getElementById("roller");
     var roller = new Roller(roll_elem);
     roll_elem.addEventListener("mouseover", roller.spin.bind(roller));
-    debugger;
     //roller.spin();
 };
