1ede91a5b81570535a7c1e18c8efd56de76b1c24
Ryan Zabcik <ryanzabcik@gmail.com>
2014-12-02 12:33:15 -0600

    remove commented code

--- main.js
+++ main.js
@@ -2,5 +2,4 @@ window.onload = function () {
     var roll_elem = document.getElementById("roller");
     var roller = new Roller(roll_elem);
     roll_elem.addEventListener("mouseover", roller.spin.bind(roller));
-    //roller.spin();
 };
