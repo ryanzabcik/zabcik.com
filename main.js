1b5a2fcc243943fce393a16f1bc7b67d8ec6ade9
Ryan Zabcik <ryanzabcik@gmail.com>
2015-02-03 15:14:24 -0600

    add even listener for touch screens

--- main.js
+++ main.js
@@ -2,4 +2,5 @@ window.onload = function () {
     var roll_elem = document.getElementById("roller");
     var roller = new Roller(roll_elem);
     roll_elem.addEventListener("mouseover", roller.spin.bind(roller));
+    roll_elem.addEventListener("touchstart", roller.spin.bind(roller));
 };
